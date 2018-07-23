9e2a472d9364c6efc199abdf566824ae0e940ff2
jmjareno <41555771+jmjareno@users.noreply.github.com>
2018-07-22 21:45:22 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-./miner --server  us.hushmine.pro --user 0x95458a0d1fe00e99beafb4dded3f28adde21d285.jmjareno/gpu_minig/jmjf.btc@gmail.com --pec --fee 0 --pass z --port 9009
+./miner --server  us.hushmine.pro --user 0x95458a0d1fe00e99beafb4dded3f28adde21d285.jmjareno/gpu_mining/jmjf.btc@gmail.com --pec --fee 0 --pass z --port 9009
